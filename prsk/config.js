8bd7994c6c93a89b53e809ff93fee4f41d1df88a
aky05090220 <100707151+aky05090220@users.noreply.github.com>
2022-03-01 23:45:11 +0800

    Add files via upload

---
+++ prsk/config.js
@@ -0,0 +1,94 @@
+const eventDataConfig = [
+  {id: 51,
+    limited: true,
+    eventTitle: '怪盗紳士のハラハラ！？ホワイトデー',
+    frontCover: 'https://static.wikia.nocookie.net/projectsekai/images/e/e6/The_Gentleman_Thief%27s_Thrilling_White_Day%21%3F_Event_Banner.png/revision/latest/scale-to-width-down/600?cb=20220227220631',
+    link: '',
+    tooltip: '',
+  },
+  {id: 50,
+    limited: false,
+    eventTitle: 'あの日、空は遠かった',
+    frontCover: 'https://static.wikia.nocookie.net/projectsekai/images/1/19/That_Day%2C_the_Sky_Was_Far_Away_Event_Banner.png/revision/latest/scale-to-width-down/600?cb=20220217152736',
+    link: 'https://www.bilibili.com/video/BV1nr4y1r7ij?spm_id_from=333.999.0.0',
+    tooltip: '',
+  }
+]
+const cardsDataConfig = [
+  {mstId: 51,
+    cards: [
+      {
+        character: '鏡音レン',
+        pic1: 'https://static.miraheze.org/projectsekaiwiki/a/a4/Len_16_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character: '日野森志歩',
+        pic1: 'https://static.miraheze.org/projectsekaiwiki/4/46/Shiho_15_thumbnail.png',
+        pic2: 'https://static.miraheze.org/projectsekaiwiki/3/32/Shiho_15_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character: '暁山瑞希',
+        pic1: 'https://static.miraheze.org/projectsekaiwiki/5/5f/Mizuki_14_thumbnail.png',
+        pic2: 'https://static.miraheze.org/projectsekaiwiki/5/5b/Mizuki_14_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character: '東雲彰人',
+        pic1: 'https://static.miraheze.org/projectsekaiwiki/5/54/Akito_13_thumbnail.png',
+        pic2: 'https://static.miraheze.org/projectsekaiwiki/b/ba/Akito_13_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character: '天馬司',
+        pic1: 'https://static.miraheze.org/projectsekaiwiki/c/c6/Tsukasa_14_thumbnail.png',
+        pic2: 'https://static.miraheze.org/projectsekaiwiki/c/c3/Tsukasa_14_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+    ]
+  },
+  {mstId: 50,
+    cards: [
+      {
+        character:'望月穂波',
+        pic1:'https://static.miraheze.org/projectsekaiwiki/5/5b/Honami_16_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character:'天馬咲希',
+        pic1:'https://static.miraheze.org/projectsekaiwiki/d/dd/Saki_15_thumbnail.png',
+        pic2:'https://static.miraheze.org/projectsekaiwiki/9/99/Saki_15_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character:'KAITO',
+        pic1:'https://static.miraheze.org/projectsekaiwiki/3/39/KAITO_13_thumbnail.png',
+        pic2:'https://static.miraheze.org/projectsekaiwiki/9/9d/KAITO_13_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character:'星乃一歌',
+        pic1:'https://static.miraheze.org/projectsekaiwiki/1/19/Ichika_14_thumbnail.png',
+        pic2:'https://static.miraheze.org/projectsekaiwiki/1/12/Ichika_14_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+      {
+        character:'日野森志歩',
+        pic1:'https://static.miraheze.org/projectsekaiwiki/b/b4/Shiho_14_thumbnail.png',
+        pic2:'https://static.miraheze.org/projectsekaiwiki/b/ba/Shiho_14_trained_thumbnail.png',
+        link1: '',
+        link2: '',
+      },
+    ]
+  }
+]
